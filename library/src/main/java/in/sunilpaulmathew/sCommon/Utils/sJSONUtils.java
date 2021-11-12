a46b850aee35e5fe05cfee03f3de58547dc54f60
sunilpaulmathew <sunil.kde@gmail.com>
2021-11-12 20:06:16 +0100

    JSONUtils: Switch to standard API
    Signed-off-by: sunilpaulmathew <sunil.kde@gmail.com>

--- library/src/main/java/in/sunilpaulmathew/sCommon/Utils/sJSONUtils.java
+++ library/src/main/java/in/sunilpaulmathew/sCommon/Utils/sJSONUtils.java
@@ -1,36 +1,60 @@
 package in.sunilpaulmathew.sCommon.Utils;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /*
  * Created by sunilpaulmathew <sunil.kde@gmail.com> on November 10, 2021
  */
 public class sJSONUtils {
 
-    public static boolean getBoolean(JsonObject object, String booleanName) {
-        return object.get(booleanName).getAsBoolean();
+    public static boolean getBoolean(JSONObject object, String booleanName) {
+        try {
+            return object.getBoolean(booleanName);
+        } catch (JSONException ignored) {
+            return false;
+        }
     }
 
-    public static JsonArray getJSONObject(JsonObject object, String arrayName) {
-        return object.getAsJsonArray(arrayName);
+    public static JSONArray getJSONObject(JSONObject object, String arrayName) {
+        try {
+            return object.getJSONArray(arrayName);
+        } catch (JSONException ignored) {
+            return null;
+        }
     }
 
-    public static JsonObject getJSONObject(String jsonString) {
-        return JsonParser.parseString(jsonString).getAsJsonObject();
+    public static JSONObject getJSONObject(String jsonString) {
+        try {
+            return new JSONObject(jsonString);
+        } catch (JSONException ignored) {
+            return null;
+        }
     }
 
-    public static int getInt(JsonObject object, String intName) {
-        return object.get(intName).getAsInt();
+    public static int getInt(JSONObject object, String intName) {
+        try {
+            return object.getInt(intName);
+        } catch (JSONException ignored) {
+            return Integer.MIN_VALUE;
+        }
     }
 
-    public static long getLong(JsonObject object, String longName) {
-        return object.get(longName).getAsLong();
+    public static long getLong(JSONObject object, String longName) {
+        try {
+            return object.getLong(longName);
+        } catch (JSONException ignored) {
+            return Long.MIN_VALUE;
+        }
     }
 
-    public static String getString(JsonObject object, String stringName) {
-        return object.get(stringName).getAsString();
+    public static String getString(JSONObject object, String stringName) {
+        try {
+            return object.getString(stringName);
+        } catch (JSONException ignored) {
+            return null;
+        }
     }
 
 }
