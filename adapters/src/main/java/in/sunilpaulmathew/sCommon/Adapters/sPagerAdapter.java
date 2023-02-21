970617f38a2356207b4004ef5af98ab10a69cf0e
sunilpaulmathew <sunil.kde@gmail.com>
2023-02-21 23:48:00 +0100

    Import adapters
    Signed-off-by: sunilpaulmathew <sunil.kde@gmail.com>

---
+++ adapters/src/main/java/in/sunilpaulmathew/sCommon/Adapters/sPagerAdapter.java
@@ -0,0 +1,48 @@
+package in.sunilpaulmathew.sCommon.Adapters;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * Created by sunilpaulmathew <sunil.kde@gmail.com> on November 13, 2021
+ */
+public class sPagerAdapter extends FragmentPagerAdapter {
+
+    private final List<Fragment> fragmentList = new ArrayList<>();
+    private final List<String> fragmentListTitles = new ArrayList<>();
+
+    public sPagerAdapter(FragmentManager manager) {
+        super(manager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentListTitles.size();
+    }
+
+    @Override
+    public int getItemPosition(@NonNull Object object) {
+        return POSITION_NONE;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return fragmentListTitles.get(position);
+    }
+    public void AddFragment(Fragment fragment, String title) {
+        fragmentList.add(fragment);
+        fragmentListTitles.add(title);
+    }
+
+}
